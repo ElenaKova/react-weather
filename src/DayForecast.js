67317f2e5a25e40a8e94a8a119a75ed504bd2d4d
EleKo <ikovalenko1999@gmail.com>
2023-03-07 14:27:33 +0100

    add forecast

--- src/DayForecast.js
+++ src/DayForecast.js
@@ -34,14 +34,16 @@ export default function ShowForecast(props) {
         <div>
             <div className="box-day" >
                 <div className="container inner-box-day text-align-center d-flex">
-                    <div className="day-of-week  align-items-center "> {day()}
+                    <div className="day-of-week px-2"> {day()}
                     </div>
-                    <div>
+                    <div className="">
                     <WeatherIcon code={props.data.weather[0].icon} size={35} />
                     </div>
                     <div className="weather-forecast-temp">
-                        <span className="weather-forecast-temp-max">Max: {maxTemp()}</span>
-                        <span className="weather-forecast-temp-min">Min: {minTemp()}</span>
+                        <span className="weather-forecast-temp-max mx-2">Max: {maxTemp()}</span>
+                        <span className = "weather-forecast-temp-min mx-2" > Min: {
+                            minTemp()
+                        } </span>
                     </div>
                 </div>
             </div>
